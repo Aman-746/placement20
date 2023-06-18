55b52f531276cdec0a87fe21a9a75773e6a214c8
Aman Kumar <83234971+Aman-746@users.noreply.github.com>
2023-06-18 08:00:04 +0530

    Added solution - LeetHub

---
+++ Max Sum without Adjacents - GFG/max-sum-without-adjacents.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// calculate the maximum sum with out adjacent
+	int findMaxSum(int *arr, int n) {
+	    // code here
+	    int prev2=0;
+	    int prev=0;
+	    for(int i=0;i<n;i++){
+	        int sum=prev2+arr[i];
+	        if(prev>sum) sum=prev;
+	        prev2=prev;
+	        prev=sum;
+	    }
+	    return prev;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.findMaxSum(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
