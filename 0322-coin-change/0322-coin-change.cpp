6f9eb03eb9e4ccd71a7404777cf11d80a9bc673c
Aman Kumar <83234971+Aman-746@users.noreply.github.com>
2023-06-19 22:22:48 +0530

    Time: 101 ms (71.62%), Space: 10.6 MB (91.21%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -4,18 +4,18 @@ public:
         int n=coins.size();
         int dp[n+1][amount+1];
         int maxi=INT_MAX-1;
-        for(int i=0;i<=amount;i++) dp[0][i]=maxi;
+        for(int j=0;j<=amount;j++) dp[0][j]=maxi;
         for(int i=1;i<=n;i++) dp[i][0]=0;
-        for(int i=1;i<=amount;i++){
-            if(i%coins[0]==0) dp[1][i]=i/coins[0];
-            else dp[1][i]=maxi;
+        for(int j=1;j<=amount;j++){
+            if(j%coins[0]==0) dp[1][j]=j/coins[0];
+            else dp[1][j]=maxi;
         }
         for(int i=2;i<=n;i++){
             for(int j=1;j<=amount;j++){
-                if(coins[i-1]<=j) dp[i][j]=min(dp[i-1][j],1+dp[i][j-coins[i-1]]);
-                else dp[i][j]=dp[i-1][j];
+                if(coins[i-1]>j) dp[i][j]=dp[i-1][j];
+                else dp[i][j]=min(1+dp[i][j-coins[i-1]],dp[i-1][j]);
             }
         }
-        return dp[n][amount]==maxi ? -1 : dp[n][amount];
+        return dp[n][amount]==maxi? -1 : dp[n][amount];
     }
 };
