fc03eaf46dcb521dee949ef731235497d6bada9f
Aman Kumar <83234971+Aman-746@users.noreply.github.com>
2023-06-29 21:41:35 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -5,36 +5,29 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-  // visited and pathVisited two array will be needed to check
-  // when we will traverse the dfs for the first time then pathVis[node] will be 1 and after all dfs call when 
-  // we come back then we will make pathVis[node]=0... if the adjacent node is in the same path means we are going
-  // in the cycle
-  bool dfs(int node, vector<int>& vis, vector<int>& pathVis, vector<int> adj[]){
-      vis[node]=1;
-      pathVis[node]=1;
-      for(auto it:adj[node]){
-          // if not visited till now
-          if(!vis[it]){
-              if(dfs(it,vis,pathVis,adj)) return true;
-          }
-          // if in the same path means there is cycle
-          else if(pathVis[it]==1) return true;
-      }
-      pathVis[node]=0;
-      return false;
-  }
-  
     // Function to detect cycle in a directed graph.
     bool isCyclic(int V, vector<int> adj[]) {
         // code here
-        vector<int>vis(V,0);
-        vector<int>pathVis(V,0);
-        for(int i=0;i<V;i++){
-            if(!vis[i]){
-                if(dfs(i,vis,pathVis,adj)) return true;
-            }
-        }
-        return false;
+        int indegree[V]={0};
+	    for(int i=0;i<V;i++){
+	        for(auto it: adj[i]) indegree[it]++;
+	    }
+	    queue<int>q;
+	    for(int i=0;i<V;i++){
+	        if(indegree[i]==0) q.push(i);
+	    }
+	    int cnt=0;
+	    while(!q.empty()){
+	        int node=q.front();
+	        q.pop();
+	        cnt++;
+	        for(auto it:adj[node]){
+	            indegree[it]--;
+	            if(indegree[it]==0) q.push(it);
+	        }
+	    }
+	    if(cnt==V) return false;
+	    return true;
     }
 };
 
