9bd48fce1e7b72c79fbf6b6976a8fa25b6ca4f0f
Aman Kumar <83234971+Aman-746@users.noreply.github.com>
2023-06-30 01:08:19 +0530

    Time: 218 ms (48.14%), Space: 58.9 MB (39.56%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    // through topological sort, we have to first reverse the graph
+    // we are reversing so that we can easily get indegree of node 
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n=graph.size();
+        vector<int>indegree(n,0);
+        vector<int>adj[n];
+        for(int i=0;i<n;i++){
+            for(auto it:graph[i]){
+                adj[it].push_back(i);
+                indegree[i]++;
+            }
+        }
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0) q.push(i);
+        }
+        vector<int>safeNodes;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            safeNodes.push_back(node);
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0) q.push(it);
+            }
+        }
+        sort(safeNodes.begin(),safeNodes.end());
+        return safeNodes;
+    }
+};
